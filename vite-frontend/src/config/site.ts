ac404044c4cab5f25f169977891236b988307488
qaq <1937228092@qq.com>
2025-08-20 16:40:58 +0800

    修改统一名称

--- vite-frontend/src/config/site.ts
+++ vite-frontend/src/config/site.ts
@@ -4,7 +4,7 @@ export type SiteConfig = typeof siteConfig;
 
 // 缓存相关常量
 const CACHE_PREFIX = 'vite_config_';
-const VERSION = "1.2.11";
+const VERSION = "1.3.0";
 
 
 const getInitialConfig = () => {
